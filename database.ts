b6470686f7df3bc47b24ce84ba21acdb7a915d53
nacretion <nacretion@gmail.com>
2023-04-10 20:31:48 +0500

    tablecreating

--- database.ts
+++ database.ts
@@ -23,6 +23,8 @@ export class Database {
     public static async initialiseTables(){
         await Database.query("create table if not exists task( id serial primary key, heading text not null, description text, end_date timestamp not null, create_date timestamp default now(), upd_date timestamp default now(), priority integer default 0, status integer default 0, creator_id integer not null, responsible_id integer not null);")
         await Database.query("create table if not exists users( id serial primary key, login text not null unique, password text not null, first_name text not null, last_name text not null, second_name text not null);")
+        await Database.query("create table if not exists responsible_rel(id serial primary key, chief_id integer not null, subordinate_id integer not null);")
+
 
     }
 }
